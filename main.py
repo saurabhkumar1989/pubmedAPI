4deb53b778cc77954d414ba205f023d961c2bb4c
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2016-10-02 20:23:47 -0400

    major changes

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import requests # that module need to be install
 from xml.etree import ElementTree
-base_url  = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
-query  = "email=saurabhkumar.spsu@gmail.com&db=pubmed&term=science%5bjournal%5d+AND+breast+cancer+AND+2008%5bpdat%5d"
-url = base_url + query
+from time import sleep
+
+email = "email=saurabhkumar.spsu@gmail.com"
+
+base_url  = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb="
+
+base_date = "2002-01-01"#year-month-date
+
+until = "2002-01-02"#year-month-date
+
+# To get the document ID
+
+#Dont even think of to change below
+name_space = '{http://www.openarchives.org/OAI/2.0/}'
+
+
+#for document Id's
+
+document_id_verb = "ListIdentifiers"
+
+metadataPrefix_doc_id = "pmc_fm"
+
+url1 = base_url + document_id_verb +"&"+ "from=" + base_date + "&" + "until=" + until + "&" + "metadataPrefix=" + metadataPrefix_doc_id
+
+doc_id_response = requests.get(url1)
 
 response = requests.get(url)
 import re
